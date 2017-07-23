881eb1b947d4e5c31768adab9dd1b0d10ea96ef1
Dess96 <dessalfaro@gmail.com>
2017-07-23 16:21:16 -0600

    Add files via upload

---
+++ Alumno1.java
@@ -0,0 +1,12 @@
+/*Objeto tipo Alumno para la clase Grupo
+ * Autora: Denisse Alfaro
+ * 2017*/
+public class Alumno1{
+  
+  private String nombre;
+  private String apellidos;
+  private String dni;
+  private String telefono;
+  
+  
+}
