e71c816d1f721eb8049b05d031ac4e9c95c5fe4f
Dess96 <dessalfaro@gmail.com>
2017-07-23 16:17:50 -0600

    Add files via upload

---
+++ EjemploArrays.java
@@ -0,0 +1,15 @@
+/*Arrays y matrices
+ * Autora: Denisse Alfaro
+ * 2017*/
+public class EjemploArrays{
+  
+  public static void main(String[] args){
+    
+    int[] array = new int[10];
+    array[3] = 5;
+    
+    int[][] matriz = new int[5][3];
+    matriz[2][2] = 4;
+  }
+  
+}
